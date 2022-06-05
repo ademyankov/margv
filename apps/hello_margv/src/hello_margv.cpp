5a95b2d640f85b8eb4a047b8296103c084527820
Alex Demyankov <alex.demyankov@gmail.com>
2022-06-05 12:35:18 -0400

    Add argv code and example app

--- apps/hello_margv/src/hello_margv.cpp
+++ apps/hello_margv/src/hello_margv.cpp
@@ -1,6 +1,53 @@
+#include <stdio.h>
+
+#include <margv/margv.hpp>
+
+static
+void usage()
+{
+    printf(
+        "usage: Session [-d] [-l LIFETIME-SECONDS] [-addr SERVER-ADDRESS] [-message MSG]\n"
+        "   {-setup [SERVER-ADDRESS] | -server | -check | -remove} [EPS-NAME]\n\n"
+        "These are common Session commands used in various situations:\n\n"
+        "working with an XPN session\n"
+        "   server [EPS-NAME]         Create a new XPN server instance on localhost with\n"
+        "                             EPS-NAME specified as an optional parameter; if not\n"
+        "                             specified the default 'EPS0' is used\n"
+        "\n"
+    );
+
+    exit(1);
+}
 
 int main(int argc, char** argv)
 {
+    margv::argv arg;
+
+    arg.add_optional("server");
+    arg.add_optional("setup");
+    arg.add_optional("check");
+    arg.add_optional("remove");
+
+    arg.add_required("message");
+
+    arg.add("addr", "https://ec2-34-209-177-147.us-west-2.compute.amazonaws.com:8443/stsxpnserver-1.0-SNAPSHOT/stsxpn-servlet");
+    arg.add("f", "b");
+    arg.add("l", "0");
+    arg.add("d");
+
+    if (arg.parse(argc, argv)) usage();
+
+    auto server = arg.get<bool>("server");
+    auto setup  = arg.get<bool>("setup");
+    auto check  = arg.get<bool>("check");
+    auto remove = arg.get<bool>("remove");
+    auto debug  = arg.get<bool>("d");
+
+    using namespace std;
+
+    auto address  = arg.get<string>("addr");
+    auto message  = arg.get<string>("message");
+    auto flags    = arg.get<string>("f");
 
     return 0;
 }
--- include/margv/margv.hpp
+++ include/margv/margv.hpp
@@ -8,4 +8,98 @@
 // found in the LICENSE file.
 //
 
+#pragma once
 
+#include <map>
+#include <string>
+
+namespace margv {
+
+    namespace detail {
+        template <typename T> T get(bool, const std::string&, const std::string&) {}
+        template <> inline bool get<bool>(bool is, const std::string&, const std::string&) { return is; }
+        template <> inline std::string get<std::string>(bool is, const std::string& got, const std::string& def) { return is ? got : def; }
+        template <> inline int get<int>(bool is, const std::string& got, const std::string& def) {
+            return (int)strtoul(detail::get<std::string>(is, got, def).c_str(), NULL, 0);
+        }
+    }
+
+    class argv {
+    public:
+        enum class ArgValue { NotRequired, Required, Optional };
+
+    public:
+        argv() = default;
+        ~argv() = default;
+
+        int parse(int argc, char** argv) {
+            int exclusive{};
+            for (int i = 1; i < argc; ++i) {
+                if (argv[i][0] != '-') continue;
+
+                auto it = kvm_.find(&argv[i][1]);
+                if (it == kvm_.end()) continue;
+
+                // mark as present
+                std::get<0>(it->second) = true;
+
+                if (std::get<4>(it->second)) ++exclusive;
+                if (std::get<3>(it->second) == ArgValue::NotRequired) continue;
+                if (std::get<3>(it->second) == ArgValue::Required)
+                    if (++i >= argc || argv[i][0] == '-') return -1;
+                else if (std::get<3>(it->second) == ArgValue::Optional) {
+                    if (i + 1 >= argc || argv[i + 1][0] == '-') continue;
+                    ++i;
+                } else return -1;
+
+                // store the switch's parameter
+                std::get<1>(it->second) = argv[i];
+            }
+            return exclusive ? 0 : -1;
+        }
+
+        // Add an optional & exclusive parameter
+        int add_optional(const std::string& arg) {
+            return add(arg, "", ArgValue::Optional, true);
+        }
+
+        // Add a required & exclusive parameter
+        int add_required(const std::string& arg) {
+            return add(arg, "", ArgValue::Required, true);
+        }
+
+        int add(const std::string& arg, const std::string& default_value, ArgValue param = ArgValue::NotRequired, bool exclusive = false) {
+            if (kvm_.find(arg) != kvm_.end()) return -1;
+            kvm_[arg] = std::make_tuple(false, std::string(""), default_value, param, exclusive);
+            return 0;
+        }
+
+        int add(const char* arg, const char* default_value) {
+            return add(arg, default_value, ArgValue::Required, false);
+        }
+
+        int add(const char* arg) {
+            return add(arg, "", ArgValue::NotRequired, false);
+        }
+
+        int add(const char* arg, bool exclusive) {
+            return add(arg, "", ArgValue::NotRequired, exclusive);
+        }
+
+        int add(const char* arg, ArgValue param, bool exclusive) {
+            return add(arg, "", param, exclusive);
+        }
+
+        template <typename T> T get(const std::string& arg) {
+            if (kvm_.find(arg) == kvm_.end()) return detail::get<T>(false, "", "");
+            auto it = kvm_[arg];
+            return detail::get<T>(std::get<0>(it), std::get<1>(it), std::get<2>(it));
+        }
+
+    private:
+        //               present    -name       default     param  exclusive
+        typedef std::tuple<bool, std::string, std::string, ArgValue, bool> ex_;
+        std::map<std::string, ex_> kvm_;
+    };
+
+}
